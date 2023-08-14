4c9b881fc6a5502186aa94b7ae8064007d1c090a
David Tigre Moraes <tigredavid007@gmail.com>
2023-08-13 23:10:39 -0300

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by David Tigre Moraes on 13/08/23.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
